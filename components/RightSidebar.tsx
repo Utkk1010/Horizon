32712993bcef5847a7ca63687caf278b1a64a170
Utkk1010 <utkarshutk10@gmail.com>
2024-11-11 22:47:23 +0530

    addon-3.2

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import BankCard from './BankCard'
 
 const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
   return (
     <aside className='right-sidebar'>
-
         <section className='flex flex-col pb-8'>
             <div className='profile-banner'/>
             <div className='profile'>
@@ -22,35 +22,46 @@ const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
                 </div>
             </div>
         </section>
-
-        <section className='banks'>
+        
+        <section className="banks">
             <div className='flex w-full justify-between'>
-                <h2 className='header-2 '>My Banks</h2>
+                <h2 className='header-2'>My Banks</h2>
                 <Link href="/" className='flex gap-2'>
-                  <Image 
-                    src="/icons/plus.svg"
-                    width={20}
-                    height={20}
-                    alt='plus'
-                  />
-                  <h2 className='text-14 font-semibold text-gray-600'>Add Bank</h2>
+                <Image 
+                src="/icons/plus.svg"
+                height={20}
+                width={20}
+                alt='plus'
+                />
+                <h2 className='text-14 font-semibold text-gray-600'>
+                    Add Bank
+                </h2>
                 </Link>
             </div>
 
-            {banks?.length >0 && }
-            <div className='relative flex flex-col flex-1 items-center justify-center gap-5'>
-                <div className='relative z-10'>
-                    BANK CARD 1
-                </div>
-                {banks[1] && (
-                    <div className='absolute right-0 top-8 z-0 w-[90%]'>
-                        BANK CARD 2
+            {banks?.length > 0 &&(
+                <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
+                    <div className='relative z-10'>
+                        <BankCard 
+                          key={banks[0].$id}
+                          account={banks[0]}
+                          userName={`${user.firstName} ${user.lastName}`}
+                          showBalance={false}
+                        />
                     </div>
-                )}
-            </div>
-
+                    {banks[1] && (
+                        <div className='absolute right-0 top-8 z-0 w-[90%]'>
+                            <BankCard 
+                              key={banks[1].$id}
+                              account={banks[1]}
+                              userName={`${user.firstName} ${user.lastName}`}
+                              showBalance={false}
+                            />
+                        </div>
+                    )}
+                </div>
+            )}
         </section>
-
     </aside>
   )
 }
