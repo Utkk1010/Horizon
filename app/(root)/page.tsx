d96577e24d0b25d4b77ba11a70b63f0babc937a9
Utkk1010 <utkarshutk10@gmail.com>
2024-11-27 20:40:47 +0530

    Implement Plaid link and dwolla

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page}}: SearchParamProps) => {
 const loggedIn = await getLoggedInUser();
+const accounts = await getAccounts({ userId: loggedIn.$id})
+
+if(!accounts) return;
+const accountsData= accounts?.data;
+const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+const account = await getAccount({ appwriteItemId })
 
   return (
     <section className="home">
@@ -14,14 +22,14 @@ const loggedIn = await getLoggedInUser();
           <HeaderBox
            type="greeting"
            title="Welcome"
-           user={loggedIn?.name || 'Guest'}
+           user={loggedIn?.firstName || 'Guest'}
            subtext="Access and manage your account and transactions efficiently."
           />
 
           <TotalBalanceBox
-           accounts={[]}
-           totalBanks={1}
-           totalCurrentBalance={2125.75}
+           accounts={accountsData}
+           totalBanks={accounts?.totalBanks}
+           totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
         Recent Transactions
@@ -29,8 +37,8 @@ const loggedIn = await getLoggedInUser();
 
       <RightSidebar 
        user={loggedIn}
-       transactions={[]}
-       banks={[{currentBalance: 120.50}, {currentBalance:200.50}]}
+       transactions={accounts?.transactions}
+       banks={accountsData.slice(0, 2)}
       />
     </section>
   )
