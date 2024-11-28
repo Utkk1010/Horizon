bfab47a843ee9ed9f93ab97d63ffdaff44f5d3ef
Utkk1010 <utkarshutk10@gmail.com>
2024-11-28 20:29:26 +0530

    Tabs and Table

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox';
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
@@ -6,8 +7,9 @@ import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
 const Home = async ({ searchParams: { id, page}}: SearchParamProps) => {
-const loggedIn = await getLoggedInUser();
-const accounts = await getAccounts({ userId: loggedIn.$id})
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id})
 
 if(!accounts) return;
 const accountsData= accounts?.data;
@@ -32,7 +34,13 @@ const account = await getAccount({ appwriteItemId })
            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        Recent Transactions
+        
+        <RecentTransactions 
+        accounts = {accountsData}
+        transactions = {account?.transactions}
+        appwriteItemId = {appwriteItemId}
+        page = {currentPage}
+        />
       </div>
 
       <RightSidebar 
